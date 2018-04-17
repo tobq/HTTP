fe94cb671ff0a2c309ac2443b5b0dcf82da2f32b
Tobi <tobi.akiny@gmail.com>
2018-04-17 20:07:53 +0100

    Update methods.js

--- methods.js
+++ methods.js
@@ -23,3 +23,18 @@ function Ajax(method, url, data, cb) {
     xmlHttp.send(data);
     return xmlHttp;
 }
+// function Post(url, data, cb) {
+//         var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.open("POST", url, !!cb);
+//         if (data.constructor === Object) {
+//             xmlHttp.setRequestHeader("Content-Type", "application/json");
+//             data = JSON.stringify(data);
+//         }
+//         else xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+//         if (typeof cb === "function") xmlHttp.onload = function () {
+//             if (this.readyState === 4) cb(this);
+//         };
+//         xmlHttp.send(data);
+//         return xmlHttp;
+//     }
